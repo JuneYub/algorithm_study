54178fc3abcd68f1d85564c4573796fd33110d0c
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-02-20 12:15:14 +0900

    [Gold IV] Title: 알파벳, Time: 828 ms, Memory: 15176 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,6 +2,7 @@
 import java.util.*;
 import java.io.*;
 
+
 class Position {
 	int y;
 	int x;
@@ -16,7 +17,8 @@ public class Main {
 
 	static int r, c;
 	static int resultCnt = 0;
-	static char[][] map;
+	static int[][] map;
+	static boolean[] visited = new boolean[26]; // 알파벳 중복체크 배열
 	static int[] dx = { 0, 1, 0, -1};
 	static int[] dy = {-1, 0, 1, 0};
 	public static void main(String args[]) throws Exception {
@@ -26,23 +28,31 @@ public class Main {
 	    r = Integer.parseInt(st.nextToken());
 	    c = Integer.parseInt(st.nextToken());
 	    
-	    map = new char[r][c];
+	    map = new int[r][c];
 	    
 	    for(int y = 0; y < r; y++) {
 	    	String str = br.readLine();
 	    	for(int x = 0; x < c; x++) {
-	    		map[y][x] = str.charAt(x);
+	    		map[y][x] = str.charAt(x) - 'A'; // A 가 들어오면 0 B가 들어오면 1
 	    	}
 	    }
 	    
-	    List<Character> list = new ArrayList<>();
-	    list.add(map[0][0]);
-	    dfs(0, 0, 1, new ArrayList<>(list));
+	    visited[map[0][0]] = true;
+	    dfs(0, 0, 1);
+	    
 	    System.out.println(resultCnt);
 	    
 	} // end of main
 	
-	public static void dfs(int y, int x, int cnt, List<Character> list) {
+	
+	/**
+	 * 
+	 * @param y 좌표
+	 * @param x
+	 * @param cnt 지나온 횟수
+	 * @param list // 중복체크하는 list를 파라미터로 넘겨줌
+	 */
+	public static void dfs(int y, int x, int cnt) {
 		
 		if(resultCnt < cnt) {
 			resultCnt = cnt;
@@ -53,10 +63,10 @@ public class Main {
 			int newY = dy[i] + y;
 			
 			if(isRange(newY, newX)) {
-				if(!list.contains(map[newY][newX])) {
-					list.add(map[newY][newX]);
-					dfs(newY, newX, cnt+1, new ArrayList<>(list));
-					list.remove(list.size()-1);
+				if(!visited[map[newY][newX]]) {
+					visited[map[newY][newX]] = true;
+					dfs(newY, newX, cnt+1);
+					visited[map[newY][newX]] = false;
 				}
 			}
 		}
