00296318ffb3bbc6eac4513025826256aa1285e4
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-03-16 15:07:16 +0900

    [Gold V] Title: 탑, Time: 852 ms, Memory: 100540 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,64 +1,64 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 
 class Tower {
-	int height;
-	int index;
-	
-	public Tower(int height, int index) {
-		this.height = height;
-		this.index = index;
-	}
+    int height;
+    int index;
+
+    Tower(int height, int index) {
+        this.height = height;
+        this.index = index;
+    }
 }
 
-public class Main {
-	
-	static int n;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		Stack<Tower> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < n; i++) {
-			int height = Integer.parseInt(st.nextToken());
-			
-			if(stack.isEmpty()) {
-				sb.append("0 ");
-				stack.push(new Tower(height, i+1));
-			} else {
-				while(true) {
-					// 스택이 비어있으면 정답을 추가하고 새로운 타워를 스택에 넣자.
-					if(stack.isEmpty()) {
-						sb.append("0 ");
-						stack.push(new Tower(height, i+1));
-						break;
-					}
-					
-					// 스택이 비어있지 않다면
-					else {
-						Tower tower = stack.peek();
-						
-						// 스택에서 꺼낸 탑의 높이가 현재 비교할 탑 높이보다 크다면
-						if(tower.height > height) {
-							// 현재 비교할 탑은 스택에 있는 탑에 레이저를 쏠 것이기 때문에
-							// 현재 위치의 정답은 스택에 있는 탑의 위치이다.
-							sb.append(tower.index + " ");
-							stack.push(new Tower(height, i+1));
-							break;
-						} else {
-							// 스택에서 꺼낸 탑의 높이가 현재 비교할 탑 높이보다 작다면
-							// 스택에 있는 탑은 이제 쓸모가 없다 지워주자.
-							stack.pop();
-						}
-					}			
-				}
-			}
-		} // end of for
-		System.out.println(sb.toString());
-	}
-	    
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        Stack<Tower> stack = new Stack<>();
+        
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < n; i++) {
+
+            int nowTowerHeight = Integer.parseInt(st.nextToken());
+            
+            if(stack.isEmpty()) {
+                sb.append("0 ");
+                stack.push(new Tower(nowTowerHeight, i+1));
+            }
+            else {
+                while(true) {
+
+                    
+
+                    if(stack.isEmpty()) {
+                        sb.append("0 ");
+                        stack.push(new Tower(nowTowerHeight, i+1));
+                        break;
+                    }
+                    else {
+                        
+                      Tower preTower = stack.peek();
+                      // System.out.println(preTower.height + " vs " + nowTowerHeight);
+                      if(preTower.height > nowTowerHeight) {
+                          sb.append(preTower.index + " ");
+                          stack.push(new Tower(nowTowerHeight, i+1));
+                          break;
+                      } else {
+                          stack.pop();
+                      }
+                    }
+                }
+            }
+        } // ============== end of for
+        
+        System.out.println(sb.toString());
+    }
 }
-    
