40792f42330f35c52f299a9ce7deeccb02c3081e
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-03-30 17:00:56 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 244 ms, Memory: 24288 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -1,178 +1,146 @@
-
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-
-
 class Oper {
-	int r;
-	int c;
-	int s;
-	
-	Oper(int r, int c,int s) {
-		this.r = r;
-		this.c = c;
-		this.s = s;
-	}
+    int r;
+    int c;
+    int s;
+
+    Oper(int r, int c, int s) {
+        this.r = r;
+        this.c = c;
+        this.s = s;
+    }
 }
 
-public class Main {
-	
-	static int n, m, k;
-	static int[][] map;
-	static int[][] result;
-	static Oper[] operArr;
-	static Oper[] resultOperArr;
-	static int[][] rotationMap;
-	static boolean[] recursionVisited;
-	static int min = Integer.MAX_VALUE;
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		// 원본
-		map = new int[n][m];
-		rotationMap = new int[n][m];
-		result = new int[n][m];
-		operArr = new Oper[k];
-		resultOperArr = new Oper[k];
-		recursionVisited = new boolean[k];
-		
-		for(int y = 0; y < n; y++) {
-			st = new StringTokenizer(br.readLine());
-			for(int x = 0; x < m; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
-				rotationMap[y][x] = map[y][x];
-			}
-		}
-		
-		for(int i = 0; i < k; i++) {
-			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
-			operArr[i] = new Oper(r, c, s);
-		}
-		
-		recursion(0);
-		
-		System.out.println(min);
-		
-		
-	} // end of main =============================
-	
-	public static void rotation(int r, int c, int s) {
-		int leftTopX = c-s-1;
-		int leftTopY = r-s-1;
-		int rightBottomX = c+s-1;
-		int rightBottomY = r+s-1;
-		int width = m;
-		int height = n;
-		
-		while(rightBottomY > leftTopY && rightBottomX > leftTopX) {
-			for(int x = leftTopX; x <= rightBottomX; x++) {
-				move(x, leftTopY, leftTopX, rightBottomX, leftTopY , rightBottomY); 
-			}
-			
-			for(int x = leftTopX; x <= rightBottomX; x++) {
-				move(x, rightBottomY, leftTopX, rightBottomX, leftTopY , rightBottomY);
-			}
-			
-			for(int y = leftTopY; y <= rightBottomY; y++) {
-				move(leftTopX, y, leftTopX, rightBottomX, leftTopY , rightBottomY);
-			}
-			
-			for(int y = leftTopY; y <= rightBottomY; y++) {
-				move(rightBottomX, y, leftTopX, rightBottomX, leftTopY , rightBottomY);
-			}
-			
-			leftTopX++;
-			leftTopY++;
-			rightBottomX--;
-			rightBottomY--;
-		}
-		
-		for(int y = 0; y < n; y++) {
-			for(int x = 0; x < m; x++) {
-				rotationMap[y][x] = result[y][x];
-			}
-		}
-	}
-	
-	public static void move(int x, int y, int startX, int endX, int startY, int endY) {
-		int r = 1;
-		int cnt = 0;
-		int tmp = rotationMap[y][x];
-		while(cnt != r) {
-			while(y == startY && x != endX && cnt != r) {
-				x++;
-				cnt++;
-			}
-			
-			while(y != endY && x == endX && cnt != r) {
-				y++;
-				cnt++;
-			}
-			
-			while(y == endY && x != startX && cnt != r) {
-				x--;
-				cnt++;
-			}
-			
-			while(y != startY && x == startX && cnt != r) {
-				y--;
-				cnt++;
-			}
-			result[y][x] = tmp;
-		}
-	}
-	
-	public static void recursion(int depth) {
-		if(depth == k) {
-			
-			
-			for(int y = 0; y < n; y++) {
-				for(int x = 0; x< m; x++) {
-					result[y][x] = rotationMap[y][x];
-				}
-			}
-			
-			for(int i = 0; i < k; i++) {
-				Oper oper = resultOperArr[i];
-				rotation(oper.r, oper.c, oper.s);
-			}
-			
-			for(int y = 0; y < n; y++) {
-				int tmp = 0;
-				for(int x = 0; x < m; x++) {
-					tmp += result[y][x];
-				}
-				//System.out.println(tmp);
-				min = Math.min(tmp, min);
-			}
-			
-			for(int y = 0; y<n; y++) {
-				for(int x =0; x<m;x++) {
-					rotationMap[y][x] = map[y][x];
-				}
-			}
-			
-			return;
-		}
-		else {
-			for(int i = 0; i < k; i++) {
-				if(!recursionVisited[i]) {
-					recursionVisited[i] = true;
-					resultOperArr[depth] = operArr[i];
-					recursion(depth+1);
-					recursionVisited[i] = false;
-				}
-			}
-		}
-		
-		
-	}
-	
+class Main {
+
+    static int n, m, k;
+    static int[][] map;
+    static int[][] resultMap;
+    static Oper[] opers;
+    static Oper[] operSeq;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+    
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        resultMap = new int[n][m];
+        
+        opers = new Oper[k];
+        operSeq = new Oper[k];
+        visited = new boolean[k];
+        
+        for(int  y = 0; y < n; y++) {
+            st = new StringTokenizer(br.readLine());
+            for(int x = 0; x < m; x++) {
+                map[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int tmpR = Integer.parseInt(st.nextToken());
+            int tmpC = Integer.parseInt(st.nextToken());
+            int tmpS = Integer.parseInt(st.nextToken());            
+            opers[i] = new Oper(tmpR, tmpC, tmpS);
+        }
+
+        permutation(0);
+        System.out.println(min);
+    }
+
+    // 순열 함수
+    public static void permutation(int depth) {
+        if(depth == k) {
+            // k개의 명령 순서를 모두 정한 경우
+
+            // 원본 맵을 복사한다.
+            for(int y = 0; y < n; y++) {
+                for(int x = 0; x < m; x++) {
+                    resultMap[y][x] = map[y][x];
+                }
+            }
+
+            // 명령에 맞춰 지도를 돌린다.
+            for(int i = 0; i < k; i++) {
+                rotation(operSeq[i].r, operSeq[i].c, operSeq[i].s);
+            }
+
+            // 돌아간 지도의 결과를 확인한다.
+            for(int y = 0; y < n; y++) {
+                int tmp = 0;
+                for(int x = 0; x < m; x++) {
+                    tmp += resultMap[y][x];
+                }
+
+                min = Math.min(tmp, min);
+            }
+
+            // 결과를 도출했으니 return 해줘야한다.
+            return;
+        }
+
+        else {
+            for(int i = 0; i < k; i++) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    operSeq[depth] = opers[i];
+                    permutation(depth + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
+    // 돌리는 함수
+    public static void rotation(int r, int c, int s) {
+        // 가장 위쪽
+        int top = r - s - 1;
+        int bottom = r + s - 1;
+        int left = c - s  - 1;
+        int right = c + s - 1;
+
+        while(top < bottom && left < right) {
+            // 좌측 상단 값을 빼놓는다.
+            int tmp = resultMap[top][left];
+
+            // 맨 왼쪽 열에서 한칸씩 위로 옮김 - 고정된 값 left
+            for(int y = top; y < bottom; y++) {
+                resultMap[y][left] = resultMap[y+1][left]; 
+            }
+      
+            // 아래쪽 행에서 왼쪽으로 한칸씩 옮김 - 고정된 값 bottom
+            for(int x = left; x < right; x++) {
+                resultMap[bottom][x] = resultMap[bottom][x+1];
+            }
+            
+            // 맨 오른쪽 열에서 한칸씩 아래로 옮김 - 고정된 값 right
+            for(int y = bottom; y > top; y--) {
+                resultMap[y][right] = resultMap[y-1][right];
+            }
+
+            // 위쪽 행에서 오른쪽으로 한칸씩 옮김
+            for(int x = right; x > left; x--) {
+                resultMap[top][x] = resultMap[top][x-1];
+            }
+
+            // 1, 1 지점에 임시 보관한 값을 넣는다.
+            resultMap[top][left+1] = tmp;
+
+            // 안쪽으로 한칸 들어간다.
+            top++;
+            left++;
+            bottom--;
+            right--;
+        }
+    }
 }
