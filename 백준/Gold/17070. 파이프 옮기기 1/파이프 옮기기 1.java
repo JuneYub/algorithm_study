49695abd887ceaa23ef6c45f73cef2e787f8b382
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-04-13 18:14:43 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,141 +2,55 @@
 import java.util.*;
 import java.io.*;
 
-class Position {
-	int x;
-	int y;
-	
-	Position(int y, int x) {
-		this.x = x;
-		this.y = y;
-	}
-	
-}
-
-class Pipe {
-	Position p1;
-	Position p2;
-	// 0 이면 가로, 1이면 세로, 2이면 대각선
-	int direction;
-	
-	Pipe(Position p1, Position p2, int direction) {
-		this.p1 = p1;
-		this.p2 = p2;
-		this.direction = direction;
-	}
-
-}
-
 public class Main {
 
-	static int n, result;
-	static int[][] map;
-    public static void main(String args[]) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	n = Integer.parseInt(br.readLine());
-    	map = new int[n][n];
-    	
-    	StringTokenizer st;
-    	
-    	for(int y = 0; y < n; y++) {
-    		st = new StringTokenizer(br.readLine());
-    		for(int x = 0; x < n; x++) {
-    			map[y][x] = Integer.parseInt(st.nextToken());
-    		}
-    	}
-    	
-    	result = 0;
-    	Queue<Pipe> q = new ArrayDeque<>();
-    	q.add(new Pipe(new Position(0, 0), new Position(0, 1), 0));
-    	
-    	while(!q.isEmpty()) {
-    		
-    		if(map[n-1][n-1] == 1 ) {
-    			break;
-    		}
-    		
-    		Pipe pipe = q.poll();
-    		int curX = pipe.p2.x;
-    		int curY = pipe.p2.y;  		
-    		int direction = pipe.direction;
-    		if(curX == n-1 && curY == n-1) {
-    			result++;
-    			continue;
-    		}
-    		
-    		if(direction == 0) {
-    			if(가로한칸체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY, curX+1), 0));
-    			}
-    			
-    			if(대각선체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY+1,curX+1), 2));
-    			}
-    		} 
-    		
-    		if(direction == 1) {
-    			if(세로한칸체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY+1, curX), 1));
-    			}
-    			
-    			if(대각선체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY+1,curX+1), 2));
-    			}
-    		} 
-    		
-    		if(direction == 2) {
-    			if(가로한칸체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY, curX+1), 0));
-    			}
-    			
-    			if(세로한칸체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY+1, curX), 1));
-    			}
-    			
-    			if(대각선체크(curY, curX)) {
-    				q.add(new Pipe(new Position(curY, curX), new Position(curY+1,curX+1), 2));
-    			}
-    		} 
-    		
-    	} // end of while
-    	
-    	System.out.println(result);
-    	
-    	
-    }
-    
-    public static boolean isRange(int y, int x) {
-    	if( y < 0 || y >= n || x < 0 || x >= n) return false;
-    	return true;
-    }
-    
-    public static boolean 가로한칸체크(int y, int x) {
-    	if(isRange(y, x+1) && map[y][x+1] == 0) {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    public static boolean 대각선체크(int y, int x) {
-    	int newY = y+1;
-    	int newX = x+1;
-    	
-    	if(isRange(newY, newX) && isRange(y, x+1) && isRange(y+1, x)) {
-    		if(map[y][x+1] == 0 && map[newY][newX] == 0 && map[y+1][x] == 0) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    
-    public static boolean 세로한칸체크(int y, int x) {
-    	if(isRange(y+1, x) && map[y+1][x] == 0) {
-    		return true;
-    	}
-    	return false;
-    }
-    
-
-
-
+	static int n;
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[n+1][n+1];
+		
+		StringTokenizer st;
+		for(int y = 0; y < n; y++) {
+			st = new StringTokenizer(br.readLine());
+			for(int x = 0; x < n; x++) {
+				map[y][x] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+		// dp y,x,3방향 
+		int dp[][][] = new int[n+1][n+1][3];
+		
+		
+		dp[0][1][0] = 1;
+		for(int y = 0; y < n; y++) {
+			for(int x = 0; x < n; x++) {
+				
+				// 우측 방향으로 이동
+				if(map[y][x+1] != 1) {
+					dp[y][x+1][0] += dp[y][x][0];
+					dp[y][x+1][0] += dp[y][x][2];
+				}
+				//System.out.println(dp[y][x+1][0]);
+				
+				// 세로 방향으로 이동
+				if(map[y+1][x] != 1) {
+					dp[y+1][x][1] += dp[y][x][1];
+					dp[y+1][x][1] += dp[y][x][2]; 
+				}
+				//System.out.println(dp[y+1][x][1]);
+				// 대각선 방향으로 이동
+				if(map[y+1][x+1] != 1 && map[y+1][x] != 1 && map[y][x+1] != 1) {
+					dp[y+1][x+1][2] = dp[y][x][0] + dp[y][x][1] + dp[y][x][2];
+				}
+				//System.out.println(dp[y+1][x+1][2]);
+			}
+		}
+		
+		System.out.println(dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2]);
+		
+		
+	}
 }
