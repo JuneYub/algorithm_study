cded00da0a4799d4e749b94eb693aa70068f89d6
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-04-21 00:42:43 +0900

    [Gold IV] Title: 두 동전, Time: 104 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.java
+++ 백준/Gold/16197. 두 동전/두 동전.java
@@ -1,81 +1,82 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+
+class Main {
     static int N, M;
     static char[][] board;
     static boolean[][][][] visited;
-    static int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     static class Coin {
-        int x1, y1, x2, y2, cnt;
-        Coin(int x1, int y1, int x2, int y2, int cnt) {
+        int x1, y1, x2, y2, move;
+
+        Coin(int x1, int y1, int x2, int y2, int move) {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
-            this.cnt = cnt;
+            this.move = move;
         }
     }
+    
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         board = new char[N][M];
         visited = new boolean[N][M][N][M];
 
-        int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+        int[] coins = new int[4];
+        int idx = 0;
 
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++) {
             String line = br.readLine();
-            for (int j = 0; j < M; j++) {
+            for(int j = 0; j < M; j++) {
                 board[i][j] = line.charAt(j);
-                if (board[i][j] == 'o') {
-                    if (x1 == -1) {
-                        x1 = i;
-                        y1 = j;
-                    } else {
-                        x2 = i;
-                        y2 = j;
-                    }
-                    board[i][j] = '.'; // 꼭 동전 자리 비워줘야 함
+                if(board[i][j] == 'o') {
+                    coins[idx++] = i;
+                    coins[idx++] = j;
                 }
             }
         }
 
-        System.out.println(bfs(x1, y1, x2, y2));
+        System.out.println(bfs(coins[0], coins[1], coins[2], coins[3]));
+        
     }
 
     static int bfs(int x1, int y1, int x2, int y2) {
-        Queue<Coin> q = new LinkedList<>();
-        q.offer(new Coin(x1, y1, x2, y2, 0));
+        Queue<Coin> q = new ArrayDeque<>();
+        q.add(new Coin(x1, y1, x2, y2, 0));
         visited[x1][y1][x2][y2] = true;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             Coin now = q.poll();
 
-            if (now.cnt >= 10) return -1;
+            if(now.move >= 10) continue;
 
-            for (int d = 0; d < 4; d++) {
+            for(int d = 0; d < 4; d++) {
                 int nx1 = now.x1 + dx[d];
                 int ny1 = now.y1 + dy[d];
                 int nx2 = now.x2 + dx[d];
                 int ny2 = now.y2 + dy[d];
 
-                boolean out1 = isOut(nx1, ny1);
-                boolean out2 = isOut(nx2, ny2);
+                boolean out1 = isRange(nx1, ny1);
+                boolean out2 = isRange(nx2, ny2);
 
-                // 둘 다 떨어지면 안 됨
+                // 둘 다 떨어지면 안됨
                 if (out1 && out2) continue;
 
-                // 하나만 떨어지면 성공
-                if (out1 || out2) return now.cnt + 1;
+                // 하나만 떨어졌으면 성공
+                if (out1 || out2) return now.move + 1;
 
-                // 벽이면 이동하지 않음
+                // 벽이면 제자리
                 if (!out1 && board[nx1][ny1] == '#') {
                     nx1 = now.x1;
                     ny1 = now.y1;
@@ -85,18 +86,17 @@ public class Main {
                     ny2 = now.y2;
                 }
 
-                // 이미 방문한 상태면 스킵
-                if (visited[nx1][ny1][nx2][ny2]) continue;
-
-                visited[nx1][ny1][nx2][ny2] = true;
-                q.offer(new Coin(nx1, ny1, nx2, ny2, now.cnt + 1));
+                // 방문하지 않은 상태만 큐에 추가
+                if (!visited[nx1][ny1][nx2][ny2]) {
+                    visited[nx1][ny1][nx2][ny2] = true;
+                    q.add(new Coin(nx1, ny1, nx2, ny2, now.move + 1));
+                }
             }
         }
-
         return -1;
     }
-
-    static boolean isOut(int x, int y) {
+    
+    static boolean isRange(int x, int y) {
         return x < 0 || x >= N || y < 0 || y >= M;
     }
 }
