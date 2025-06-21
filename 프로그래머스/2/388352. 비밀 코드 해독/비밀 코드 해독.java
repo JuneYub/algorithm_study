343b0466ee2dfa363c4b5679a1e81378f27799bf
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-06-22 00:49:30 +0900

    [level 2] Title: 비밀 코드 해독, Time: 39.72 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 class Solution {
     
-    int count = 0;
     int n;
+    int count = 0;
     int[][] q;
     int[] ans;
     
@@ -18,34 +18,31 @@ class Solution {
         return count;
     }
     
-    void dfs(int start, List<Integer> picked) {
+    public void dfs(int start, List<Integer> picked) {
         if(picked.size() == 5) {
-            if(isValid(picked)) {
+            if(isValid(picked)) 
                 count++;
-            }
             return;
         }
         
-        for(int i =  start; i <= n; i++) {
+        for(int i = start; i <= n; i++) {
             picked.add(i);
-            dfs(i+1, picked);
+            dfs(i + 1, picked);
             picked.remove(picked.size() - 1);
         }
     }
     
-
-    
-    boolean isValid(List<Integer> picked) {
+    public boolean isValid(List<Integer> picked) {
+        
         for(int i = 0; i < q.length; i++) {
-            int match = 0;
+            int match = 0;   
             for(int num : q[i]) {
-                if(picked.contains(num)) {
-                    match++;
-                }
+                if(picked.contains(num)) match++;
             }
             if(match != ans[i]) return false;
         }
         return true;
     }
+    
 }
 
