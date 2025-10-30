2c8d45298d8a6e3059252d99cb2517b2242bcc91
JuneYub <91084695+JuneYub@users.noreply.github.com>
2025-10-30 23:03:09 +0900

    [Silver II] Title: N과 M (9), Time: 296 ms, Memory: 38068 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,64 +1,64 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
 
-	static int n,m;
-	static BufferedWriter bw;
-	
-	static Integer[] arr;
-	static boolean[] visited;
-	static int[] result;
-	static List<int[]> arrayList = new ArrayList<>();
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		arr = new Integer[n];
-		visited = new boolean[n];
-		result = new int[m];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr, new Comparator<Integer>() {
-			public int compare(Integer o1, Integer o2) {
-				return o1 - o2;
-			};
-		});
-		
-		recursion(0);
-		bw.flush();
-	}
-	
-	static void recursion(int depth) throws Exception {
-		if(depth == m) {
-			
-			for(int i : result) {
-				bw.write(i + " ");
-			}
-			bw.newLine();
-			return;
-		}
-		
-		int beforeNum = 0;
-		for(int i = 0; i < n; i++) {
-			if(beforeNum != arr[i] && !visited[i]) {
-				beforeNum = arr[i];
-				visited[i] = true;
-				result[depth] = arr[i];
-				recursion(depth + 1);
-				visited[i] = false;
-			}
+class Main {
 
-		}
-	}
+    static int n, m;
+    static List<Integer> list = new ArrayList<>();
+    static Set<String> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        dfs(new ArrayList<Integer>(), 0);
+
+        System.out.print(sb.toString());
+        
+    }
+
+    public static void dfs(List<Integer> answerList, int depth) {
+        if(answerList.size() == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(answerList.get(i) + " ");
+            }
+            sb.append("\n");
+        }
+
+        int before = -1;
+        for(int i = 0; i < list.size(); i++) {
+            if(!visited[i] && before != list.get(i)) {
+                visited[i] = true;
+                answerList.add(list.get(i));
+                before = list.get(i);
+                dfs(answerList, depth + 1);
+                visited[i] = false;
+                answerList.remove(answerList.size() - 1);
+            }
+
+            
+        }
+
+        
+        
+    }
+
 
 }
